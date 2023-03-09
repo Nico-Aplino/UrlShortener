4e9b3bacdf0606b08598476f5f3df43c30d41c04
Nico-Aplino <110082469+Nico-Aplino@users.noreply.github.com>
2023-03-09 15:01:42 +0800

    Added validations.

---
+++ UrlShortener.Domain/Validations/UrlValidator.cs
@@ -0,0 +1,22 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UrlShortener.BLL.Models;
+
+namespace UrlShortener.BLL.Validations
+{
+    public class UrlValidator : AbstractValidator<UrlModel>
+    {
+        public UrlValidator()
+        {
+            RuleFor(x => x.OriginalUrl)
+                .NotEmpty().WithMessage("The URL must not be empty.")
+                .Must(url => Uri.TryCreate(url, UriKind.Absolute, out var uriResult) &&
+                             (uriResult.Scheme == Uri.UriSchemeHttp || uriResult.Scheme == Uri.UriSchemeHttps))
+                .WithMessage("The URL must be a valid HTTP or HTTPS URL.");
+        }
+    }
+}
