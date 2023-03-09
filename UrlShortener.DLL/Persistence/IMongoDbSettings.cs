78374b0d07458acb883f2811859d9ba05002f893
Nico-Aplino <110082469+Nico-Aplino@users.noreply.github.com>
2023-03-09 11:42:31 +0800

    Added persistence.

---
+++ UrlShortener.DLL/Persistence/IMongoDbSettings.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UrlShortener.DLL.Persistence
+{
+    public interface IMongoDbSettings
+    {
+        public string ConnectionString { get; set; } 
+        public string DatabaseName { get; set; }
+    }
+}
